4db8590e25e4d76542011a8800e4dea43fdbb237
FilbertWijayaa <filbert.wijaya.004@gmail.com>
2024-11-08 13:30:22 +0700

    p5-6

--- pos/api/urls.py
+++ pos/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from api import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from .views import (
+    TableRestoListApiView, TableRestoDetailApiView,
+)
+app_name='api'
 
-app_name = 'api'
-urlpattern = [
-    #path('api/v1/login', LoginView.as_view()),
-    #path('api/v1/logout', LogoutView.as_view()),
-    #path('api/v1/register', RegisterWaitressAPI.as_view()),
+urlpatterns = [
+    #path('api/vl/login', LoginView.as_view()),
+    #path('api/vl/logiout', LogoutView.as_view()),
+    #path('api/vl/register', RegisterWaitressApi.as_view()),
+    path('api/table_resto', views.TableRestoListApiView.as_view()),
+    path('api/table_resto/<int:id>', views.TableRestoDetailApiView.as_view()),
 ]
--- pos/pos/urls.py
+++ pos/pos/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from pos_app import views
 from api import views
 from django.conf import settings
